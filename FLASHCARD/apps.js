7f1f79d3cc054b147e30a75b135959da05c24e11
Bia <140396915+biagds15@users.noreply.github.com>
2024-09-24 08:41:57 -0300

    24.09

---
+++ FLASHCARD/apps.js
@@ -0,0 +1,11 @@
+function criaCartao (categoria, pergunta, resposta) {
+    let container  = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    let respostaEstaVisivel = false
+} 
+function viraCartao(){
+    respostaestaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+    cartao.addEventListener('click', viraCartao)
+}
